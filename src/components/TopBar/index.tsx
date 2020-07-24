5b070d4bd03b1bc5dedc1c320afa71901e5bff8a
JelteMX <jelte.lagendijk@mendix.com>
2020-07-24 14:52:16 +0200

    Updates

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -12,6 +12,7 @@ import {
     Select,
     MenuItem,
     Link,
+    SvgIcon,
 } from "@material-ui/core";
 
 import FileCopyOutlinedIcon from "@material-ui/icons/FileCopyOutlined";
@@ -111,6 +112,21 @@ const TopBar: FC<{ onHomeClick: () => void }> = observer(({ onHomeClick }) => {
                         <Link href="https://docs.mendix.com/apidocs-mxsdk/apidocs/design-properties" target="_blank">
                             <IconButton aria-label="Info">
                                 <HelpOutlineOutlinedIcon fontSize="small" className={classes.toolBarIcon} />
+                            </IconButton>
+                        </Link>
+                    </Tooltip>
+                    <Tooltip title="Info">
+                        <Link href="https://github.com/JelteMX/mendix-dpt" target="_blank">
+                            <IconButton aria-label="Info">
+                                <SvgIcon viewBox="0 0 30.47 29.72" style={{ width: "20px" }}>
+                                    <path
+                                        d={
+                                            "M15.23,0a15.24,15.24,0,0,0-4.81,29.69c.76.14,1-.33,1-.73s0-1.32,0-2.59c-4.24.92-5.13-2.05-5.13-2.05a4.07,4.07,0,0,0-1.7-2.23c-1.38-.94.11-.92.11-.92a3.19,3.19,0,0,1,2.33,1.57A3.25,3.25,0,0,0,11.49,24a3.28,3.28,0,0,1,1-2c-3.38-.39-6.94-1.69-6.94-7.53a5.93,5.93,0,0,1,1.57-4.09,5.51,5.51,0,0,1,.15-4s1.28-.41,4.19,1.56a14.52,14.52,0,0,1,7.63,0c2.91-2,4.18-1.56,4.18-1.56a5.48,5.48,0,0,1,.16,4A5.92,5.92,0,0,1,25,14.44C25,20.29,21.39,21.58,18,22a3.66,3.66,0,0,1,1,2.82c0,2,0,3.68,0,4.18s.28.88,1,.73A15.24,15.24,0,0,0,15.23,0Z"
+                                        }
+                                        fill="#FFF"
+                                        //style="fill:#191717;fill-rule:evenodd"
+                                    />
+                                </SvgIcon>
                             </IconButton>
                         </Link>
                     </Tooltip>
